c482e4e3fad81757437d6c7f3a3bf0a9e392610d
Fabrice Geib <fabricegeib@gmail.com>
2022-03-23 23:57:48 +0100

    Component Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,20 @@
 // import React from 'react'
+import PropTypes from 'prop-types'
 
 const Button = ({ color, text }) => {
 	return (
-		<button className="btn">{text}</button>
+		<button style={{ backgroundColor: color }} className="btn">{text}</button>
 	)
 }
 
 Button.defaultProps = {
+	color: 'steelblue',
 	text: 'Button',
+}
+
+Button.propTypes = {
+	color: PropTypes.string,
+	text: PropTypes.string,
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ title }) => {
 		<header className='header'>
 			<h1 className='header__title'>{title}</h1>
 			{/* <button className='header__button'>Button</button> */}
-			<Button color='grey' text='Add' />
+			<Button color='lightskyblue' text='Add' />
 		</header>
 	)
 }
