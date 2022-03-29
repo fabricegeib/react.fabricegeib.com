8ce289c0cf4860e7491b834af2051ab06d71a645
Fabrice Geib <fabricegeib@gmail.com>
2022-03-29 15:03:15 +0200

    install supabase

---
+++ src/supabaseClient.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+// export const supabase = createClient(
+// 	"xxx",
+// 	"xxx"
+// )
