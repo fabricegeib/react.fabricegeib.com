9f96f5ea239de15d0ff5d381bb91a2f4c7427594
Fabrice Geib <fabricegeib@gmail.com>
2022-04-12 19:47:17 +0200

    tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,51 @@
-const tasks = [
-	{
-		id: 1,
-		text: 'first task',
-		day: '25 mars à 12h00',
-		reminder: true,
-	},
-	{
-		id: 2,
-		text: 'second task',
-		day: '25 mars à 13h00',
-		reminder: false,
-	},
-	{
-		id: 3,
-		text: 'three task',
-		day: '25 mars à 14h00',
-		reminder: true,
-	}
-]
+// const tasks = [
+// 	{
+// 		id: 1,
+// 		text: 'first task',
+// 		day: '25 mars à 12h00',
+// 		reminder: true,
+// 	},
+// 	{
+// 		id: 2,
+// 		text: 'second task',
+// 		day: '25 mars à 13h00',
+// 		reminder: false,
+// 	},
+// 	{
+// 		id: 3,
+// 		text: 'three task',
+// 		day: '25 mars à 14h00',
+// 		reminder: true,
+// 	}
+// ]
+
+import { useState } from "react"
 
 const Tasks = () => {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: 'learn react',
+			day: '25 mars à 12h00',
+			reminder: true,
+		},
+		{
+			id: 2,
+			text: 'second task',
+			day: '25 mars à 13h00',
+			reminder: false,
+		},
+		{
+			id: 3,
+			text: 'three task',
+			day: '25 mars à 14h00',
+			reminder: true,
+		}
+	])
+
 	return (
-		<div>
-			{tasks.map((task) => (<h3>{task.text}</h3>))}
+		<div className="container">
+			{tasks.map((task) => (<h3 key={task.id} >{task.text}</h3>))}
 		</div>
 	)
 }
